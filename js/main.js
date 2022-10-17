d04021f329b46714ac7e1a0048f99fd5b961ff79
hramovsky <54127359+hramovsky@users.noreply.github.com>
2022-10-17 11:25:28 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,106 @@
+
+// Get a reference to a CSInterface object
+var csInterface = new CSInterface();
+
+
+
+var button = window.document.getElementById("btn1");
+button.onclick = function() {
+    csInterface.evalScript("script1()");
+};
+
+var button = window.document.getElementById("btn2");
+button.onclick = function() {
+   csInterface.evalScript("script2()");
+};
+
+var button = window.document.getElementById("btn3");
+button.onclick = function() {
+    csInterface.evalScript("script3()");
+};
+
+var button = window.document.getElementById("btn4");
+button.onclick = function() {
+    csInterface.evalScript("script4()");
+};
+
+var button = window.document.getElementById("btn5");
+button.onclick = function() {
+    csInterface.evalScript("script5()");
+};
+
+var button = window.document.getElementById("btn6");
+button.onclick = function() {
+    csInterface.evalScript("script6()");
+};
+
+var button = window.document.getElementById("btn7");
+button.onclick = function() {
+    csInterface.evalScript("script7()");
+};
+
+var button = window.document.getElementById("btn8");
+button.onclick = function() {
+    csInterface.evalScript("script8()");
+};
+
+var button = window.document.getElementById("btn9");
+button.onclick = function() {
+    csInterface.evalScript("script9()");
+};
+
+var button = window.document.getElementById("btn10");
+button.onclick = function() {
+    csInterface.evalScript("script10()");
+};
+
+var button = window.document.getElementById("btn11");
+button.onclick = function() {
+    csInterface.evalScript("script11()");
+};
+
+var button = window.document.getElementById("btn12");
+button.onclick = function() {
+   csInterface.evalScript("script12()");
+};
+
+var button = window.document.getElementById("btn13");
+button.onclick = function() {
+    csInterface.evalScript("script13()");
+};
+
+var button = window.document.getElementById("btn14");
+button.onclick = function() {
+    csInterface.evalScript("script14()");
+};
+
+var button = window.document.getElementById("btn15");
+button.onclick = function() {
+    csInterface.evalScript("script15()");
+};
+
+var button = window.document.getElementById("btn16");
+button.onclick = function() {
+    csInterface.evalScript("script16()");
+};
+
+var button = window.document.getElementById("btn17");
+button.onclick = function() {
+    csInterface.evalScript("script17()");
+};
+
+var button = window.document.getElementById("btn18");
+button.onclick = function() {
+    csInterface.evalScript("script18()");
+};
+
+var button = window.document.getElementById("btn19");
+button.onclick = function() {
+    csInterface.evalScript("script19()");
+};
+
+var button = window.document.getElementById("btn20");
+button.onclick = function() {
+    csInterface.evalScript("script20()");
+};
+

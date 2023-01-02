8362b11b732982b3c74c3647461b388704bd6332
hramovsky <54127359+hramovsky@users.noreply.github.com>
2023-01-01 16:59:15 -0800

    Update linkscript.jsx

--- jsx/linkscript.jsx
+++ jsx/linkscript.jsx
@@ -1,42 +1,212 @@
-var path = '/c/AE_Scripts/'
+var path = '/d/Design/AE Scripts/'
+
+var file_01 = 'rd_CompSetter.jsx';
+var file_02 = 'BG Renderer.jsxbin';
+var file_03 = 'Move Anchor Point_ui.jsx';
+var file_04 = 'AutoCrop_2015.jsxbin';
+var file_05 = 'Remove Effect by Name.jsx';
+var file_06 = 'Selected_Layers_Renamer.jsx';
+var file_07 = 'LayerRandomShifter.jsx';
+var file_08 = 'Paste Multiple Keyframes v2-0-3.jsxbin';
+var file_09 = 'Rift.jsxbin';
+var file_10 = 'SimpleShape v1.0 add Fill Stroke2.jsx';
+var file_11 = 'True Comp Duplicator.jsx';
+var file_12 = 'Attacher 0.1.jsx';
+var file_13 = '';
+var file_14 = '';
+var file_15 = '';
+var file_16 = '';
+var file_17 = '';
+var file_18 = '';
+var file_19 = '';
+var file_20 = '';
+var file_21 = '';
+var file_22 = '';
+var file_23 = '';
+var file_24 = '';
+var file_25 = '';
+
+
 
 function script1() {
-    var file = new File(path + 'Attacher 0.1.jsx');
-
-               file.open("r");
-
-               eval(file.read());
-
-               file.close();
+    var file = new File(path + file_01);
+    file.open("r");
+    eval(file.read());
+    file.close();
 }
 
 function script2() {
-    var file = new File(path + 'LayerRandomShifter.jsx');
-
-               file.open("r");
-
-               eval(file.read());
-
-               file.close();
+    var file = new File(path + file_02);
+    file.open("r");
+    eval(file.read());
+    file.close();
 }
 
 function script3() {
-    var file = new File(path + 'Move Anchor Point.jsx');
-
-               file.open("r");
-
-               eval(file.read());
-
-               file.close();
+    var file = new File(path + file_03);
+    file.open("r");
+    eval(file.read());
+    file.close();
 }
 
 function script4() {
-    var file = new File(path + 'Remove Effect by Name.jsx');
+    var file = new File(path + file_04);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
 
-               file.open("r");
+function script5() {
+    var file = new File(path + file_05);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
 
-               eval(file.read());
+function script6() {
+    var file = new File(path + file_06);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
 
-               file.close();
+function script7() {
+    var file = new File(path + file_07);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
+
+function script8() {
+    var file = new File(path + file_08);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
+
+function script9() {
+    var file = new File(path + file_09);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
+
+function script10() {
+    var file = new File(path + file_10);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
+
+function script11() {
+    var file = new File(path + file_11);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
+
+function script12() {
+    var file = new File(path + file_12);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
+
+function script13() {
+    var file = new File(path + file_13);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
+
+function script14() {
+    var file = new File(path + file_14);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
+
+function script15() {
+    var file = new File(path + file_15);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
+
+function script16() {
+    var file = new File(path + file_16);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
+
+function script17() {
+    var file = new File(path + file_17);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
+
+function script18() {
+    var file = new File(path + file_18);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
+
+
+function script19() {
+    var file = new File(path + file_19);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
+
+
+function script20() {
+    var file = new File(path + file_20);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
+
+
+function script21() {
+    var file = new File(path + file_21);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
+
+
+function script22() {
+    var file = new File(path + file_22);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
+
+
+function script23() {
+    var file = new File(path + file_23);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
+
+
+function script24() {
+    var file = new File(path + file_24);
+    file.open("r");
+    eval(file.read());
+    file.close();
+}
+
+
+function script25() {
+    var file = new File(path + file_25);
+    file.open("r");
+    eval(file.read());
+    file.close();
 }
 
